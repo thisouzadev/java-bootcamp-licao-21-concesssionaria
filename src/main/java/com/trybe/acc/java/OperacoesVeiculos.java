b1752f70f7acd0aa43d8fd951bd36e54b0d9c444
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-04-19 19:27:11 -0300

    finish

--- src/main/java/com/trybe/acc/java/OperacoesVeiculos.java
+++ src/main/java/com/trybe/acc/java/OperacoesVeiculos.java
@@ -1,5 +1,19 @@
 package com.trybe.acc.java;
 
-public class OperacoesVeiculos {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+public class OperacoesVeiculos {
+  public <T> int numVeiculosDiferentesVendidos(Stream<T> veiculosVendidos) {
+    return (int) veiculosVendidos.distinct().count();
+  }
+
+  public <T> Stream<String> obterRepresentacao(Stream<T> veiculos) {
+    return veiculos.map(e -> e.toString());
+  }
+
+  public <T> List<T> obterLista(Stream<T> veiculos) {
+    return veiculos.collect(Collectors.toList());
+  }
 }
